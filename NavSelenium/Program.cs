b4e5371fdd93c5f65268a68a8d4be50e1a127ef4
professionalsna <professionalsna@gmail.com>
2017-01-26 16:09:25 +0500

    https://github.com/professionalsna/NavSelenium.git

---
+++ NavSelenium/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace NavSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            string url = "https://emaktab.org/demo/login.aspx";
+            //driver.Url = url;
+            driver.Navigate().GoToUrl(url);
+
+
+            IWebElement element = driver.FindElement(By.Id("btnUserLogin"));
+            element.Click();
+
+            //Or can be written as
+            //driver.FindElement(By.LinkText("ToolsQA")).Click();
+
+        }
+    }
+}
